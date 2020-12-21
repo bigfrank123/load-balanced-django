066b501fa6abef95190058dbc87dc9b10d5a2b6f
Ryan <ryan.honer@aa.com>
2020-12-21 13:36:15 -0700

    first commit

---
+++ swnTools/swnTools/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for swnTools project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swnTools.settings')
+
+application = get_asgi_application()
